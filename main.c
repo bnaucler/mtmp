dff576c93591306e5db52e305a385175e1163687
bnaucler <mail@bnaucler.se>
2017-06-13 22:19:49 +0200

    Complete refactoring

--- main.c
+++ main.c
@@ -5,14 +5,17 @@
 int main(int argc, char **argv) {
 
 	char loc[LOCLEN] = "";
-	char ret[WBUFSZ];
 	char *ip = NULL;
+	weather wtr;
 
 	if(argc > 1) strncpy(loc, argv[1], LOCLEN);
 	else ip = creq(IPECHO);
 
-	mtmp(loc, ip, ret, WBUFSZ);
-	printf("%s", ret);
+	mtmp(loc, ip, &wtr);
+
+	printf("%s, %s: %s %.1fC, humidity: %d%%, %d hPa, %.1f m/s %s\n",
+			wtr.loc, wtr.cc, wtr.desc, wtr.temp, wtr.hum, wtr.pres,
+			wtr.ws, wtr.wdir);
 
 	return 0;
 }
